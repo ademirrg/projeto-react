443f089c0075f95271574368e52675048d881fcb
Ademir Rocha Garcia <ademir_rg@yahoo.com.br>
2025-08-13 17:09:50 -0300

    insercao de imagem no body

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,5 +1,12 @@
 function Body() {
-    return <h2>Teste Body</h2>
+
+const imgPath = "~/Downloads/mgs2.jpg"
+    return (
+        <div>
+        <h2>Teste Body</h2>
+        <img width={100} src={imgPath} />
+        </div>
+    )
 }
 
 export default Body
